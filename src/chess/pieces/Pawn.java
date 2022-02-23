a1ebb2ab991b97e867ee94e5bad8e553d13832d3
joelvitorniino <joelprofessor13@gmail.com>
2022-02-23 20:36:03 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -16,7 +16,7 @@ public class Pawn extends ChessPiece {
 
         Position p = new Position(0, 0);
 
-        if (getColor() == Color.WHITE) {
+        if(getColor() == Color.WHITE) {
             p.setValues(position.getRow() - 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
@@ -45,7 +45,7 @@ public class Pawn extends ChessPiece {
             }
 
             p.setValues(position.getRow() + 2, position.getColumn());
-            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
